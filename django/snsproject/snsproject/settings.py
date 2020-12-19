293cae1010ddfc61ef9b5da456b5655fc1dc74eb
FTakahashi <ftakahashi@gr8-bizhack.com>
2020-12-19 12:03:30 +0900

    sqliteからpostgresに変更

--- django/snsproject/snsproject/settings.py
+++ django/snsproject/snsproject/settings.py
@@ -74,12 +74,24 @@ WSGI_APPLICATION = 'snsproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'boarddb',
+        'USER': 'postgres',
+        'PASSWORD' : 'password',
+        'HOST' : 'srvname.local',
+        'PORT' : 5432,
     }
 }
+
 
 
 # Password validation
